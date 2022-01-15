9129bfbf69adbad2f966dcd8d71159b205feff11
汉广 <serfend@foxmail.com>
2022-01-15 16:08:43 +0800

    chore[common]directive update

--- src/directive/waves/index.js
+++ src/directive/waves/index.js
@@ -1,5 +1,4 @@
 import waves from './waves'
-
 const install = function(Vue) {
   Vue.directive('waves', waves)
 }
--- src/directive/waves/waves.js
+++ src/directive/waves/waves.js
@@ -3,46 +3,61 @@ import './waves.css'
 const context = '@@wavesContext'
 
 function handleClick(el, binding) {
+  const elements = []
   function handle(e) {
-    const customOpts = Object.assign({}, binding.value)
     const opts = Object.assign({
       ele: el, // 波纹作用元素
       type: 'hit', // hit 点击位置扩散 center中心点扩展
       color: 'rgba(0, 0, 0, 0.15)' // 波纹颜色
-    },
-    customOpts
-    )
+    }, binding.value)
     const target = opts.ele
-    if (target) {
-      target.style.position = 'relative'
-      target.style.overflow = 'hidden'
-      const rect = target.getBoundingClientRect()
-      let ripple = target.querySelector('.waves-ripple')
-      if (!ripple) {
-        ripple = document.createElement('span')
-        ripple.className = 'waves-ripple'
-        ripple.style.height = ripple.style.width = Math.max(rect.width, rect.height) + 'px'
-        target.appendChild(ripple)
-      } else {
-        ripple.className = 'waves-ripple'
-      }
-      switch (opts.type) {
-        case 'center':
-          ripple.style.top = rect.height / 2 - ripple.offsetHeight / 2 + 'px'
-          ripple.style.left = rect.width / 2 - ripple.offsetWidth / 2 + 'px'
-          break
-        default:
-          ripple.style.top =
-            (e.pageY - rect.top - ripple.offsetHeight / 2 - document.documentElement.scrollTop ||
-              document.body.scrollTop) + 'px'
-          ripple.style.left =
-            (e.pageX - rect.left - ripple.offsetWidth / 2 - document.documentElement.scrollLeft ||
-              document.body.scrollLeft) + 'px'
-      }
-      ripple.style.backgroundColor = opts.color
-      ripple.className = 'waves-ripple z-active'
-      return false
+    if (!target) return
+    target.style.position = 'relative'
+    target.style.overflow = 'hidden'
+    const rect = target.getBoundingClientRect()
+    let i = elements.findIndex(i => !i.running)
+    let item
+    if (i === -1) {
+      item = document.createElement('span')
+      item.style.height = item.style.width = Math.max(rect.width, rect.height) + 'px'
+      item.id = new Date().getTime()
+      elements.push(item)
+      i = elements.length - 1
+      target.appendChild(item)
     }
+    item = elements[i]
+    if (!item.running_timer) {
+      item.running_timer = setTimeout(() => {
+        item.running = false
+        item.running_timer = null
+      }, 2e3)
+    }
+    if (!item.clear_timer) {
+      item.clear_timer = setTimeout(() => {
+        target.removeChild(item)
+        elements.splice(elements.findIndex(x => x.id === item.id), 1)
+      }, 10e3)
+    }
+    item.running = true
+    item.className = 'waves-ripple'
+    let y, x
+    switch (opts.type) {
+      case 'center':
+        y = rect.height / 2 - item.offsetHeight / 2
+        x = rect.width / 2 - item.offsetWidth / 2
+        break
+      default: {
+        const de = document.documentElement
+        const db = document.body
+        y = (e.pageY - rect.top - item.offsetHeight / 2 - de.scrollTop || db.scrollTop)
+        x = (e.pageX - rect.left - item.offsetWidth / 2 - de.scrollLeft || db.scrollLeft)
+      }
+    }
+    item.style.top = `${y}px`
+    item.style.left = `${x}px`
+    item.style.backgroundColor = opts.color
+    item.className = 'waves-ripple z-active'
+    return false
   }
 
   if (!el[context]) {
