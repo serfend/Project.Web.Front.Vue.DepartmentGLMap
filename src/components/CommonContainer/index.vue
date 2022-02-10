711cdca9c517f5c5c359115c1e1ab4b180cf7d21
汉广 <serfend@foxmail.com>
2022-02-10 22:20:47 +0800

    feat[common-field]container with loader data

---
+++ src/components/CommonContainer/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonContainer',
+  mounted () {
+    this.$store.dispatch('common_fields/init')
+  }
+}
+</script>
